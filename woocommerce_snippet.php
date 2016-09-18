b3a53d0c9203919e5901eacaa9ccbeebd68cf069
Muddasir Abbas <muddasirawan@gmail.com>
2016-09-18 14:47:18 +0500

    Woocommerce AJAX Cart Update
    Ajax cart updated code snippet updated.

---
+++ woocommerce_snippet.php
@@ -0,0 +1,27 @@
+<?php 
+########################################
+//	Woocommerce AJAX Cart Update Start
+########################################
+?>
+
+<!-- Place this code Where you want to see Cart data -->
+<a class="cart-contents" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a>
+
+
+<?php
+// Ensure cart contents update when products are added to the cart via AJAX (place the following in functions.php)
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	ob_start();
+	?>
+	<a class="cart-contents" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf (_n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a> 
+	<?php
+	
+	$fragments['a.cart-contents'] = ob_get_clean();
+	
+	return $fragments;
+}
+
+########################################
+//	Woocommerce AJAX Cart Update End
+########################################
